f2ad27f21648d7b47528b4a0128275c2406ca4dd
Your Name <you@example.com>
2020-02-06 22:37:30 +0800

    实现问题回复不刷新登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,21 +1,30 @@
 function post() {
-    const questionId=$("#question_id").val();
-    const comment_content=$("#comment_content").val();
+    const questionId = $("#question_id").val();
+    const comment_content = $("#comment_content").val();
     $.ajax({
-        type:"POST",
-        dataType:"json",
-        contentType:'application/json',
-        url:"/comment",
-        data:JSON.stringify({
-            "parent_id":questionId,
-            "content":comment_content,
-            "type":1
+        type: "POST",
+        dataType: "json",
+        contentType: 'application/json',
+        url: "/comment",
+        data: JSON.stringify({
+            "parent_id": questionId,
+            "content": comment_content,
+            "type": 1
         }),
-        success:function (response) {
-            if (response.code==200){
+        success: function (response) {
+            if (response.code == 200) {
                 $("#comment_section").hide();
-            }else {
-                alert(response.message);
+            } else {
+                if (response.code == 2003) {
+                    if (confirm(response.message)){
+                        window.open("https://github.com/login/oauth/authorize?client_id=3ff1b6b3e84c6f89364e&redirect_uri=http://localhost:8080/callback&scope=user&state==1");
+                        window.localStorage.setItem("closable","true");
+                    }
+
+                } else {
+                    alert(response.message);
+
+                }
             }
         },
 
